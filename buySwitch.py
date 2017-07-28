594a2faa53b6bad0e6fb27fe9de81a688524c854
nshhhin <cs172004@meiji.ac.jp>
2017-07-28 12:32:04 +0900

    update file
    コンソール上だけに通知だけする機能

--- buySwitch.py
+++ buySwitch.py
@@ -4,8 +4,40 @@
 # by nino
 # 現在人気すぎて買えないSwitchをどうにかして買いたい人のためのプログラム
 
+# ノジマオンライン
+# ノーマル: https://online.nojima.co.jp/Nintendo-HAC-S-KAAAA-%E3%80%90Switch%E3%80%91-%E3%83%8B%E3%83%B3%E3%83%86%E3%83%B3%E3%83%89%E3%83%BC%E3%82%B9%E3%82%A4%E3%83%83%E3%83%81%E6%9C%AC%E4%BD%93-Joy-Con%28L%29-%28R%29-%E3%82%B0%E3%83%AC%E3%83%BC/4902370535709/1/cd/
+# スプラ付: https://online.nojima.co.jp/Nintendo-HAC-S-KACEA-%E3%80%90Switch%E3%80%91-%E3%83%8B%E3%83%B3%E3%83%86%E3%83%B3%E3%83%89%E3%83%BC%E3%82%B9%E3%82%A4%E3%83%83%E3%83%81%E6%9C%AC%E4%BD%93-%E3%82%B9%E3%83%97%E3%83%A9%E3%83%88%E3%82%A5%E3%83%BC%E3%83%B32%E3%82%BB%E3%83%83%E3%83%88/4902370537338/1/cd/
+
+from bs4 import BeautifulSoup #スクレイピング用のライブラリ
+import urllib.request
+from urllib.request import Request, urlopen
+
+import time
+import re
+
+url_normal = "https://online.nojima.co.jp/Nintendo-HAC-S-KAAAA-%E3%80%90Switch%E3%80%91-%E3%83%8B%E3%83%B3%E3%83%86%E3%83%B3%E3%83%89%E3%83%BC%E3%82%B9%E3%82%A4%E3%83%83%E3%83%81%E6%9C%AC%E4%BD%93-Joy-Con%28L%29-%28R%29-%E3%82%B0%E3%83%AC%E3%83%BC/4902370535709/1/cd/"
+url_spla = "https://online.nojima.co.jp/Nintendo-HAC-S-KAAAA-%E3%80%90Switch%E3%80%91-%E3%83%8B%E3%83%B3%E3%83%86%E3%83%B3%E3%83%89%E3%83%BC%E3%82%B9%E3%82%A4%E3%83%83%E3%83%81%E6%9C%AC%E4%BD%93-Joy-Con%28L%29-%28R%29-%E3%82%B0%E3%83%AC%E3%83%BC/4902370535709/1/cd/"
+
 def main():
-	main()
+	loop = True
+	while( loop ):
+		time.sleep(3)
+		req = Request(url_normal, headers={'User-Agent': 'Mozilla/5.0'})
+		response = urlopen(req)
+		html = response.read()
+		soup = BeautifulSoup(html, "lxml")
+		state = soup.find("span", class_="hassoumeyasu2")
+		match = re.search('<span>(.*)</span>', str(state))
+		print(match.groups()[0]) # => これが「完売御礼」じゃなかった時は在庫がある!!
+
+		if( match.groups()[0] != "完売御礼" ):
+			print("入荷したよ!!")
+			loop = False
+
+
+
+
 
 if __name__ == '__main__':
 	main()
+
